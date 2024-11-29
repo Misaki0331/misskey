13438ec55eb36f966b0de9b8cbe1f16a1655dee5
Misaki <60120497+Misaki0331@users.noreply.github.com>
2024-11-29 18:25:52 +0900

    メールをもう少し詳細に書くように

--- packages/backend/src/server/api/SigninService.ts
+++ packages/backend/src/server/api/SigninService.ts
@@ -6,6 +6,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import * as Misskey from 'misskey-js';
 import { DI } from '@/di-symbols.js';
+import type { Config } from '@/config.js';
 import type { SigninsRepository, UserProfilesRepository } from '@/models/_.js';
 import { IdService } from '@/core/IdService.js';
 import type { MiLocalUser } from '@/models/User.js';
@@ -19,6 +20,9 @@ import type { FastifyRequest, FastifyReply } from 'fastify';
 @Injectable()
 export class SigninService {
 	constructor(
+		@Inject(DI.config)
+		private config: Config,
+
 		@Inject(DI.signinsRepository)
 		private signinsRepository: SigninsRepository,
 
@@ -50,9 +54,34 @@ export class SigninService {
 
 			const profile = await this.userProfilesRepository.findOneByOrFail({ userId: user.id });
 			if (profile.email && profile.emailVerified) {
-				this.emailService.sendEmail(profile.email, 'New login / ログインがありました',
-					'There is a new login. If you do not recognize this login, update the security status of your account, including changing your password. / 新しいログインがありました。このログインに心当たりがない場合は、パスワードを変更するなど、アカウントのセキュリティ状態を更新してください。',
-					'There is a new login. If you do not recognize this login, update the security status of your account, including changing your password. / 新しいログインがありました。このログインに心当たりがない場合は、パスワードを変更するなど、アカウントのセキュリティ状態を更新してください。');
+				this.emailService.sendEmail(profile.email, `【${this.config.url}】新規ログインのお知らせ`,
+					`${user.name??`@${user.username}`} 様<br><br>`+
+					`いつも${this.config.url}をご利用いただきありがとうございます。<br><br>`+
+					`お使いのアカウント(@${user.username})に対する新しいログインがありましたのでお知らせいたします。<br><br>`+
+					`ログイン時刻 : ${new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo',})}<br>`+
+					`IPアドレス : ${request.ip}<br><br>`+
+					`<strong>●ユーザー自身がログインした場合</strong><br>`+
+					`このメールは無視して構いません。<br>`+
+					`引き続き${this.config.url}をお楽しみください<br><br>`+
+					`<strong>●このログインに心当たりがない場合</strong><br>`+
+					`すぐにパスワードを変更とログイントークンを再生成し、アカウントを保護してください。<br><br>`+
+					`また、パスワード変更やログイン履歴等の各種情報は以下のURLから確認できます。<br>`+
+					`<a href="https://${this.config.url}/settings/security">https://${this.config.url}/settings/security</a><br><br>`+
+					`※本メールは送信専用になります。`
+					,
+					`${user.name??`@${user.username}`} 様\n\n`+
+					`いつも${this.config.url}をご利用いただきありがとうございます。\n\n`+
+					`お使いのアカウント(@${user.username})に対する新しいログインがありましたのでお知らせいたします。\n\n`+
+					`ログイン時刻 : ${new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo',})}\n`+
+					`IPアドレス : ${request.ip}\n\n`+
+					`●ユーザー自身がログインした場合\n`+
+					`このメールは無視して構いません。\n`+
+					`引き続き${this.config.url}をお楽しみください\n\n`+
+					`●このログインに心当たりがない場合\n`+
+					`すぐにパスワードを変更とログイントークンを再生成し、アカウントを保護してください。\n\n`+
+					`また、パスワード変更やログイン履歴等の各種情報は以下のURLから確認できます。\n`+
+					`https://${this.config.url}/settings/security \n\n`+
+					`※本メールは送信専用になります。`);
 			}
 		});
 
--- packages/backend/src/server/api/SignupApiService.ts
+++ packages/backend/src/server/api/SignupApiService.ts
@@ -199,9 +199,18 @@ export class SignupApiService {
 
 			const link = `${this.config.url}/signup-complete/${code}`;
 
-			this.emailService.sendEmail(emailAddress!, 'Signup',
-				`To complete signup, please click this link:<br><a href="${link}">${link}</a>`,
-				`To complete signup, please click this link: ${link}`);
+			this.emailService.sendEmail(emailAddress!, `【${this.config.url}】ユーザー登録のご案内`,
+				`${this.config.url} へようこそ！<br>`+
+				`下記URLをクリックして、ユーザー登録を完了してください。<br><br>`+
+				`メールアドレス認証用URL : <a href="${link}">${link}</a><br>`+
+				`※本メールは送信専用になります。<br>`+
+				`※本メール内容に心当たりがない場合は、メールを削除してください。`,
+
+				`${this.config.url} へようこそ！\n`+
+				`下記URLをクリックして、ユーザー登録を完了してください。\n\n`+
+				`メールアドレス認証用URL : ${link}`+
+			  `※本メールは送信専用になります。\n`+
+				`※本メール内容に心当たりがない場合は、メールを削除してください。`);
 
 			if (ticket) {
 				await this.registrationTicketsRepository.update(ticket.id, {
